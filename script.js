f21457e3995e878e4778325f35465aaf0078dda5
Ahmedsyedfazeel <sfazeel15@gmail.com>
2022-01-03 00:20:35 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function compute() {
+    var principal = document.getElementById("principal").value;
+
+    if(principal == ""){
+        alert("Please enter the positive number");
+        principal.focus();
+        return false;
+    }
+    if(principal <= 0){
+        alert("Please enter the positive number");
+        principal.focus();
+        return false;
+    }
+    //Get the values and calculate 
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var yearInTheFuture = new Date().getFullYear()+parseInt(years);
+    
+    
+    
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark> "+rate+"% </mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+yearInTheFuture+"</mark>\<br\>"
+    
+    //Create the Interest text
     
 }
-        
+
+//update ther ate value
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+
+
+
